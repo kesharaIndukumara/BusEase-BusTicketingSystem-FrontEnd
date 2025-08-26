0452a49e63d7dde46de73d76df1e70f80578c643
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-08-26 16:56:58 +0530

    style: add BusOwnerReg component and update routing in App to include registration path

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Header from './components/common/Header'
 import Login from './components/common/logIn/Login';
 import ForgotPassword from './components/common/logIn/FogotPassword';
+import Reg from './components/clientBusOwnerServices/Reg';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/header" element={<Header/>}></Route>
         <Route path="/login" element={<Login/>}></Route>
         <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+        <Route path="/reg" element={<Reg/>}></Route>
       </Routes>
     </BrowserRouter>,
     </>
