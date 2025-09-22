a7881e274575f00148fba07e380ae2b23508fc4a
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-09-22 23:40:31 +0530

    refactor: remove routing in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import ClientHome from './components/clientHome/ClientHome'
-import { BrowserRouter, Route, Routes } from "react-router";
-import Header from './components/common/Header'
-import Login from './components/common/logIn/Login';
-import ForgotPassword from './components/common/logIn/FogotPassword';
-import Reg from './components/clientBusOwnerServices/Reg';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ClientHome/>}></Route>
-        <Route path="/header" element={<Header/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
-        <Route path="/reg" element={<Reg/>}></Route>
-      </Routes>
-    </BrowserRouter>,
+      <ClientHome/>
     </>
   )
 }
